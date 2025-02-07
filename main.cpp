34f15eaf9f5f484cd9bb0fd1ea0cc6360599e3aa
Amanda <amanda.rosado3@upr.edu>
2025-02-07 18:08:05 +0000

    commit1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
+/*
+Asignación: Lab01
+Amanda Rosado Rosado
+Núm. Est: 801-23-8238
+Colaboraciones: 
+*/
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main ()
+{
+
+    cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater." << endl;
+
+
+    int movie;
+
+    cout << "Entre el nombre de su pelicula: " << endl;
+    cin >> movie;
+
+    double ticket_adulto;
+    cout << "Entre la cantidad de tickets de adultos vendidos: " << endl;
+    cin >> ticket_adulto;
+
+//cout << "Entre la cantidad de tickets de menores vendidos: " << endl;
+//cin >> ticket_menor;
+
+//total = ticket_adulto + ticket_menor;
+
+//cout << total << endl;
+
+return 0;
+}
