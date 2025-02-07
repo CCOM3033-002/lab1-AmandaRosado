8e66bc15012cf89e0eb5cbad6c26f8a8df62c2ee
Amanda <amanda.rosado3@upr.edu>
2025-02-07 20:27:38 +0000

    commit1.4

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main ()
 {
 
-    cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater." << endl;
+    cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater." << "\n" << endl;
 
 
     char movie;
@@ -23,33 +23,34 @@ int main ()
 
     cout << "Entre el nombre de su pelicula: " << endl;
     cin >> movie;
-    
+    cout << "\n";
 
     cout << "Entre la cantidad de tickets de adultos vendidos: " << endl;
     cin >> ticket_adulto;
-    
+    cout << "\n";
 
     cout << "Entre la cantidad de tickets de menores vendidos: " << endl;
     cin >> ticket_menor;
-
+    cout << "\n\n\n";
 
 
     double adulto_total = ticket_adulto * 10.0;
     double menor_total = ticket_menor * 6.0;
-    
+
     double gross_bop = adulto_total + menor_total;
     double net_porcentaje = gross_bop * 0.20;
     double net_bop = gross_bop - net_porcentaje;
 
+
     cout << "Esta es la cantidad generada en total por el cine ese dia: " << endl;
-    cout << "$" << gross_bop << ".00" << endl;
+    cout << "$" << gross_bop << ".00" << "\n" << endl;
 
 
     cout << "Esta es la cantidad generada para el distribuidor: " << endl;
-    cout << "$" << net_bop << endl;
+    cout << "$" << net_bop << "\n" << endl;
 
     cout << "Esta es la cantidad que gano el cine en ventas: " << endl;
-    cout << "$" << net_porcentaje << endl;
+    cout << "$" << net_porcentaje << "\n" << endl;
 
 
     return 0;
